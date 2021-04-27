815d055e396270c4f529274a4a954f22458619fb
Josh Searle <joshryansearle@gmail.com>
2021-04-27 23:54:09 +1000

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+const config = {
+  mode: 'production',
+  entry: path.resolve(__dirname, './src/index.ts'),
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exlude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
+
+module.exports = config;
