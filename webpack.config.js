5cdb374ed5ba51389d167981fab64a9f21d0f65c
Josh Searle <joshryansearle@gmail.com>
2021-04-28 00:02:49 +1000

    Add html to webpack and hash

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 const config = {
   mode: 'production',
   entry: path.resolve(__dirname, './src/index.ts'),
@@ -16,9 +19,15 @@ const config = {
     extensions: ['.tsx', '.ts', '.js'],
   },
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle.[hash].js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+    }),
+  ],
 };
 
 module.exports = config;
