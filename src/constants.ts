dec5a71a53a1be73c70bfbccf3d686bee5e4ddd5
Josh Searle <joshryansearle@gmail.com>
2021-04-29 17:08:06 +1000

    Add constants list

---
+++ src/constants.ts
@@ -0,0 +1,11 @@
+import Vector from './vector';
+
+const constants = {
+  ticksPerSecond: 60,
+  thrust: 60,
+  maxVelocityMagnitude: 400,
+  shipTurn: 10,
+  maxShipRpm: 40,
+};
+
+export default constants;
