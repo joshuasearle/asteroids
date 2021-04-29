69d336e6266aee361f621034c56e9bddb85aecc5
Josh Searle <joshryansearle@gmail.com>
2021-04-29 17:08:28 +1000

    Add game loop

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
 // Need this statement to import styles into build files
 import './styles.css';
+import Ship from './ship';
+import constants from './constants';
 
-window.addEventListener('load', () => {
-  alert('Hello from index.ts');
-});
+const svg = document.getElementById('svg');
+
+const ship = new Ship();
+ship.tick();
+ship.render(svg);
+
+const gameLoop = setInterval(() => {
+  ship.tick();
+  ship.render(svg);
+}, 1000 / constants.ticksPerSecond);
+
+window.addEventListener('keydown', ship.inputHandler);
