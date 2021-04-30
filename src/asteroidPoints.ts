0afec42ade72b2e3a38b4f38bc9b89b3fdb24d7f
Josh Searle <joshryansearle@gmail.com>
2021-04-30 16:00:40 +1000

    Add svg points for asteroids

---
+++ src/asteroidPoints.ts
@@ -0,0 +1,6 @@
+const asteroidPoints = {
+  bigAsteroid: '20,20 35,5 55,20 85,20 90,60 60,70 35,90 10,60 20,40',
+  smallAsteroid: '10,10 16,3 26,10 42,10 45,30 30,35 17,45 5,30 10,20',
+};
+
+export default asteroidPoints;
