f6e1535fff2d9e68271d093c3410e47f51ce26f8
Josh Searle <joshryansearle@gmail.com>
2021-04-30 15:59:12 +1000

    Add vector functionality

--- src/vector.ts
+++ src/vector.ts
@@ -3,12 +3,18 @@ export default class Vector {
   magnitude() {
     return Math.sqrt(this.x ** 2 + this.y ** 2);
   }
+  angle() {
+    return (Math.atan2(this.y, this.x) * 180) / Math.PI;
+  }
 }
 
 export const addVectors = (v1: Vector, v2: Vector): Vector => {
   return new Vector(v1.x + v2.x, v1.y + v2.y);
 };
 
+export const subVectors = (v1: Vector, v2: Vector): Vector => {
+  return new Vector(v1.x - v2.x, v1.y - v2.y);
+};
 export const scaleVector = (v: Vector, s: number): Vector => {
   return new Vector(v.x * s, v.y * s);
 };
