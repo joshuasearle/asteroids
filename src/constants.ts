78b828121e820b2e483a91d4eb563c0721d3fa65
Josh Searle <joshryansearle@gmail.com>
2021-04-30 16:01:24 +1000

    Add constants

--- src/constants.ts
+++ src/constants.ts
@@ -6,6 +6,13 @@ const constants = {
   maxVelocityMagnitude: 400,
   shipTurn: 10,
   maxShipRpm: 40,
+  bulletSpeed: 500,
+  bulletRadius: 5,
+  minAsteroidSpeed: 50,
+  maxAsteroidSpeed: 200,
+  minAsteroidRpm: -40,
+  maxAsteroidRpm: 40,
+  maxAsteroidCount: 5,
 };
 
 export default constants;
